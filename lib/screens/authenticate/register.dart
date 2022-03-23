7a5a4a296d446a1d09654d01623b4158b84d57e9
Quoc Nguyen <nganhquoc95@gmail.com>
2022-03-23 21:26:17 +0700

    Add loading

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
@@ -20,6 +21,7 @@ class _RegisterState extends State<Register> {
   String email = "";
   String password = "";
   String error = "";
+  bool loading = false;
 
   @override
   void initState() {
@@ -28,89 +30,97 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Register'),
-        elevation: 0.0,
-        actions: <Widget>[
-          TextButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Register'),
+              elevation: 0.0,
+              actions: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: Icon(Icons.person),
+                  label: Text('Sign In'),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 20,
-          horizontal: 50,
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20),
-              Text(
-                error,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.red[900],
-                ),
+            body: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 50,
               ),
-              SizedBox(height: 20),
-              TextFormField(
-                validator: emailValidator,
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  icon: Icon(Icons.email),
-                  hintText: 'E-Mail',
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                obscureText: true,
-                validator: passwordValidator,
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  icon: Icon(Icons.lock),
-                  hintText: 'Password',
-                ),
-              ),
-              SizedBox(height: 20),
-              TextButton.icon(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result = await _auth.register(email, password);
-                    print(result);
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    Text(
+                      error,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.red[900],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      validator: emailValidator,
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        icon: Icon(Icons.email),
+                        hintText: 'E-Mail',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: true,
+                      validator: passwordValidator,
+                      onChanged: (val) {
+                        setState(() {
+                          password = val;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        icon: Icon(Icons.lock),
+                        hintText: 'Password',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton.icon(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          dynamic result =
+                              await _auth.register(email, password);
+                          print(result);
 
-                    if (result == null) {
-                      setState(() {
-                        error =
-                            "The email address is already in use by another account.";
-                      });
-                    }
-                  }
-                },
-                icon: Icon(Icons.person_add),
-                label: Text('Create a Account'),
+                          if (result == null) {
+                            setState(() {
+                              error =
+                                  "The email address is already in use by another account.";
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                      icon: Icon(Icons.person_add),
+                      label: Text('Create a Account'),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
@@ -14,12 +15,13 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
   String email = "";
   String password = "";
   String error = "";
-
-  AuthService _auth = AuthService();
-  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   void onPressdSignInAsAnonymous() async {
     dynamic result = await _auth.signInAnonymus();
@@ -28,88 +30,95 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In'),
-        elevation: 0.0,
-        actions: <Widget>[
-          TextButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Sign In'),
+              elevation: 0.0,
+              actions: <Widget>[
+                TextButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 20,
-          horizontal: 50,
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20),
-              Text(
-                error,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.red[900],
+            body: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 50,
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    Text(
+                      error,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.red[900],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      validator: emailValidator,
+                      onChanged: (val) {
+                        setState(() {
+                          email = val;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        icon: Icon(Icons.email),
+                        hintText: 'E-Mail',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      validator: passwordValidator,
+                      obscureText: true,
+                      onChanged: (val) {
+                        setState(() {
+                          password = val;
+                        });
+                      },
+                      decoration: textInputDecoration.copyWith(
+                        icon: Icon(Icons.lock),
+                        hintText: 'Password',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton.icon(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error =
+                                  "The password is invalid or the user does not have a password.";
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                      icon: Icon(Icons.login),
+                      label: Text('Login'),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 20),
-              TextFormField(
-                validator: emailValidator,
-                onChanged: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  icon: Icon(Icons.email),
-                  hintText: 'E-Mail',
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                validator: passwordValidator,
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-                decoration: textInputDecoration.copyWith(
-                  icon: Icon(Icons.lock),
-                  hintText: 'Password',
-                ),
-              ),
-              SizedBox(height: 20),
-              TextButton.icon(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result =
-                        await _auth.signInWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error =
-                            "The password is invalid or the user does not have a password.";
-                      });
-                    }
-                  }
-                },
-                icon: Icon(Icons.login),
-                label: Text('Login'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
